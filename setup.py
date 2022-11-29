7cd5ca7efd87a4634db553fb360578ec16d6edbe
MichalGasiorowski <MGasiorowski82@gmail.com>
2022-11-29 15:50:49 +0100

    initial commit with project scaffolding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for Kaggle Store sales Time Series Forecasting competition',
+    author='MGasiorowski82@gmail.com',
+    license='',
+)
